80dad61d79353b34833acc835b403ac97443f7e2
mordren <joaokko@gmail.com>
2024-02-29 22:19:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("wheel", event => {
+    const delta = event.deltaY;
+    let scrollDirection = delta > 0 ? 1 : -1;
+    
+    // Encontra a seção atualmente visível
+    let currentPage = document.querySelector('.page-visible');
+    if (!currentPage) {
+      currentPage = document.querySelector('.page');
+      currentPage.classList.add('page-visible');
+    }
+    
+    // Calcula o índice da próxima página baseado na direção do scroll
+    let nextPageIndex = [...document.querySelectorAll('.page')].indexOf(currentPage) + scrollDirection;
+    let pages = document.querySelectorAll('.page');
+    if (nextPageIndex >= 0 && nextPageIndex < pages.length) {
+      // Remove a classe da página atual e adiciona à próxima
+      currentPage.classList.remove('page-visible');
+      pages[nextPageIndex].classList.add('page-visible');
+      
+      // Scroll para a próxima página
+      pages[nextPageIndex].scrollIntoView({ behavior: 'smooth' });
+    }
+  
+    event.preventDefault();
+  }, { passive: false });
