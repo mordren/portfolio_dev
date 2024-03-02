35fc525c171f838ecbe78016fd317555ce662a12
mordren <joaokko@gmail.com>
2024-03-02 00:54:15 -0300

    teste front-page

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
-document.addEventListener("wheel", event => {
-  const delta = event.deltaY;
-  let scrollDirection = delta > 0 ? 1 : -1;
-  
-  // Encontra a seção atualmente visível
+let startY = 0; // Inicializa a posição inicial do toque em Y
+
+// Evento de início de toque
+document.addEventListener("touchstart", function(e) {
+  startY = e.touches[0].clientY;
+}, { passive: false });
+
+// Evento de fim de toque
+document.addEventListener("touchend", function(e) {
+  let endY = e.changedTouches[0].clientY;
+  let deltaY = endY - startY;
+  let scrollDirection = deltaY > 0 ? -1 : 1; // Inverte a direção porque um gesto de deslizar para cima significa rolar para baixo
+
+  navigatePages(scrollDirection);
+  e.preventDefault();
+}, { passive: false });
+
+// Função para navegar entre as páginas
+function navigatePages(scrollDirection) {
   let currentPage = document.querySelector('.page-visible');
   if (!currentPage) {
     currentPage = document.querySelector('.page');
     currentPage.classList.add('page-visible');
   }
-  
-  // Calcula o índice da próxima página baseado na direção do scroll
+
   let nextPageIndex = [...document.querySelectorAll('.page')].indexOf(currentPage) + scrollDirection;
   let pages = document.querySelectorAll('.page');
   if (nextPageIndex >= 0 && nextPageIndex < pages.length) {
-    // Remove a classe da página atual e adiciona à próxima
     currentPage.classList.remove('page-visible');
     pages[nextPageIndex].classList.add('page-visible');
     
-    // Scroll para a próxima página
     pages[nextPageIndex].scrollIntoView({ behavior: 'smooth' });
   }
+}
 
+// Reutiliza a função navigatePages no evento de rolagem do mouse
+document.addEventListener("wheel", event => {
+  const delta = event.deltaY;
+  let scrollDirection = delta > 0 ? 1 : -1;
+  navigatePages(scrollDirection);
   event.preventDefault();
 }, { passive: false });
